efee5f16e314df5833a0472141041712d808829e
Bode <jry.bode@gmail.com>
2023-03-15 13:42:13 +0700

    Merge branch 'master' of https://github.com/Bode20029/numeric

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import Bisection from "./components/Bisection"
-import FalseMethod from "./components/FalseMethod"
-import OnePoint from "./components/OnePoint"
-import Taylor from "./components/Taylor"
+import Bisection from "./components/Pages/Bisection"
+import FalseMethod from "./components/Pages/FalseMethod"
+import OnePoint from "./components/Pages/OnePoint"
+import Taylor from "./components/Pages/Taylor"
 import { Route,Routes } from 'react-router-dom';
 import './App.css';
 
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path="/Bisection" element={<Bisection/>}></Route>
-      <Route path="/FalseMethod" element={<FalseMethod/>}></Route>
-      <Route path="/OnePoint" element={<OnePoint/>}></Route>
-      <Route path="/Taylor" element={<Taylor/>}></Route>
-    </Routes>
-    </>
+   // <>
+    // <Routes>
+    //   <Route path="/Bisection" element={<Bisection/>}></Route>
+    //   <Route path="/FalseMethod" element={<FalseMethod/>}></Route>
+    //   <Route path="/OnePoint" element={<OnePoint/>}></Route>
+    //   <Route path="/Taylor" element={<Taylor/>}></Route>
+    // </Routes>
+      <FalseMethod/>
+    //</>
     
 
   );
