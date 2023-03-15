d5499273cc287c347182420e7ad21b667dfb379e
Bode <jry.bode@gmail.com>
2023-03-15 21:28:01 +0700

    home

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import Bisection from "./components/Pages/Bisection"
 import FalseMethod from "./components/Pages/FalseMethod"
 import OnePoint from "./components/Pages/OnePoint"
 import Taylor from "./components/Pages/Taylor"
+import Navbarbode from "./components/Menu/Navbar"
 import { Route,Routes } from 'react-router-dom';
-import './App.css';
+
 
 
 function App() {
   return (
-   // <>
-    // <Routes>
-    //   <Route path="/Bisection" element={<Bisection/>}></Route>
-    //   <Route path="/FalseMethod" element={<FalseMethod/>}></Route>
-    //   <Route path="/OnePoint" element={<OnePoint/>}></Route>
-    //   <Route path="/Taylor" element={<Taylor/>}></Route>
-    // </Routes>
+   <div>
+   
+    <Navbarbode/>
+   {/**  <Routes>
+      <Route path="/" element={""}></Route>
+      <Route path="/Bisection" element={<Bisection/>}></Route>
+      <Route path="/FalseMethod" element={<FalseMethod/>}></Route>
+      <Route path="/OnePoint" element={<OnePoint/>}></Route>
+      <Route path="/Taylor" element={<Taylor/>}></Route>
+    </Routes>
       <FalseMethod/>
-    //</>
-    
+  **/}
+      </div>
 
   );
 }
---
+++ src/components/Menu/Navbar.js
@@ -0,0 +1,26 @@
+
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+const Navbarbode = () => {
+  return (
+    <div className='container'>
+    <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="#">HTML</a></li>
+      <li><a href="#">CSS</a></li>
+      <li><a href="#">JavaScript</a></li>
+    </ul>
+  </div>
+    </div>
+  )
+}
+
+export default Navbarbode
