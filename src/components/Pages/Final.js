9003068655ff6928a20a369a3f8c45344bc7a34a
Bode <jry.bode@gmail.com>
2023-04-12 10:34:17 +0700

    graph

--- src/components/Pages/Final.js
+++ src/components/Pages/Final.js
@@ -1,107 +1,128 @@
-import React, {useState} from 'react'
-import { Button, Container, Form, FormGroup, FormLabel, Table } from 'react-bootstrap'
-import { evaluate } from 'mathjs'
+import React, { useState } from "react";
+import { Button, Container, Form, FormGroup, FormLabel, Table } from "react-bootstrap";
+import { evaluate } from "mathjs";
+import { Scatter } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
 const Final = () => {
-    const plottable = () =>{
-        SetValueXstart(data.map((element) => element.XL));
-        SetValueXstop(data.map((element) => element.XR));
-        return(
-            <Container>
-                <Table variant='dark'>
-                    <thead>
-                        <tr>
-                            
-                            <th>x</th>
-                            <th>y</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((element, index) =>{
-                            return (
-                                <tr key={index}>
-                            <td>{element.XL}</td>
-                            <td>{element.XR}</td>
-                        </tr>)
-                        })}
-                        
-                    </tbody>
-                </Table>
-            </Container>
-        );
-    }
-    const print = (xl,xr) => {
-        
-        var fx;
-        var iter = xl;
-        var end = xr;
-        var obj = {};
-        
-        do{
-            
-            fx = evaluate(Equation, { x: iter }).toString();
-            
-            obj ={
-                XL:iter,
-                XR:fx
-            }
-            data.push(obj);
-            iter++;
-        }while(iter<=end)
-        
-        
-        
-    }
-
-    const data = [];
-    //for mapping
-    const [valueXstart, SetValueXstart] = useState([]);
-    const [valueXstop, SetValueXstop] = useState([]);
-    const [screen, SetScreen] = useState();
-    
-    const [Equation, SetEquation] = useState("(x^4)-13")
-    const [XL,setXL] = useState(0);
-    const [XR,setXR] = useState(0);
-    
-    const inputEquation = (e) => {
-        console.log(e.target.value);
-        SetEquation(e.target.value);
-    }
-    const inputXL = (e) => {
-        console.log(e.target.value);
-        setXL(e.target.value);
-    }
-    const inputXR = (e) => {
-        console.log(e.target.value);
-        setXR(e.target.value);
-    }
-    const calculate = () => {
-        const xlnum = parseFloat(XL);
-        const xrnum = parseFloat(XR);
-        print(xlnum,xrnum);
-        SetScreen(plottable());
-    }
+  const plottable = () => {
     return (
-    <div>
-        <Form>
-            <FormGroup>
-                <FormLabel> Input Equation</FormLabel>
-                <br></br>
-                <input type="text" onChange={inputEquation}  value={Equation}></input> 
-                <br></br>
-                <FormLabel>Input start</FormLabel>
-                <br></br>
-                <input type="number" onChange={inputXL}></input>
-                <br></br>
-                <FormLabel>Input stop</FormLabel>
-                <br></br>
-                <input type="number" onChange={inputXR}></input>
+      <Container>
+        <Table variant="dark">
+          <thead>
+            <tr>
+              <th>x</th>
+              <th>y</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((element, index) => {
+              return (
+                <tr key={index}>
+                  <td>{element.XL}</td>
+                  <td>{element.XR}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
+    );
+  };
 
-            </FormGroup>
-            <Button onClick={calculate}> Calculate </Button>
+  const graph = () => {
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    };
+    const dataset1 = {
+        datasets: [
+          {
+            label: 'Function Values',
+            data: data.map((point) => ({ x: point.XL, y: point.XR })),
+            backgroundColor: 'rgba(255, 99, 132, 1)',
+            pointRadius: 5
+          }
+        ]
+    };
+    return <Scatter options={options} data={dataset1} />;
+}
+  const print = (xl, xr) => {
+    let fx;
+    let iter = xl;
+    const end = xr;
+    let obj = {};
+    do {
+      fx = evaluate(Equation, { x: iter }).toString();
+      obj = {
+        XL: iter,
+        XR: fx,
+      };
+      data.push(obj);
+      iter++;
+    } while (iter <= end);
+  };
+
+  const [data, setData] = useState([]);
+  const [valueXstart, setValueXstart] = useState([]);
+  const [valueXstop, setValueXstop] = useState([]);
+  const [screen, setScreen] = useState();
+  const [Equation, setEquation] = useState("(x^4)-13");
+  const [XL, setXL] = useState(0);
+  const [XR, setXR] = useState(0);
+
+  const inputEquation = (e) => {
+    setEquation(e.target.value);
+  };
+  const inputXL = (e) => {
+    setXL(e.target.value);
+  };
+  const inputXR = (e) => {
+    setXR(e.target.value);
+  };
+  const calculate = () => {
+    const xlnum = parseFloat(XL);
+    const xrnum = parseFloat(XR);
+    print(xlnum, xrnum);
+    setValueXstart(data.map((element) => element.XL));
+    setValueXstop(data.map((element) => element.XR));
+    setScreen(<div>{plottable()}{graph()}</div>);
+  };
+
+  return (
+    <div>
+      <Form>
+        <FormGroup>
+          <FormLabel>Input Equation</FormLabel>
+          <br></br>
+          <input type="text" onChange={inputEquation} value={Equation}></input>
+          <br></br>
+          <FormLabel>Input start</FormLabel>
+          <br></br>
+          <input type="number" onChange={inputXL}></input>
+          <br></br>
+          <FormLabel>Input stop</FormLabel>
+          <br></br>
+          <input type="number" onChange={inputXR}></input>
+        </FormGroup>
+        <Button onClick={calculate}>Calculate</Button>
+     
+
         </Form>
         <br></br>
         <div> {screen} </div>
+        
     </div>
   )
 }
